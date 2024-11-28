5c342e1dbaf935543cac983cb9b884d265bec98d
João Moreira <jgmoreira1809@gmail.com>
2024-11-28 11:34:50 -0300

    hello working

---
+++ demo/src/main/java/com/example/demo/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+}
